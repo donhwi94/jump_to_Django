4863f4b6816653fc69450b0b82f0c6afbb3451cd
donhwi94 <ehsgnl123@naver.com>
2021-05-07 17:08:23 +0900

    3-11 views.py 파일 분리하기

---
+++ pybo/views/base_views.py
@@ -0,0 +1,26 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    page = request.GET.get('page', '1')
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list' : page_obj}
+
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
