5d67015ad633ea5a402e321532ded0442ded2c44
donhwi94 <ehsgnl123@naver.com>
2021-05-14 19:17:26 +0900

    4-13 로그 직접 출력하도록 설정해보기

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -194,5 +194,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
